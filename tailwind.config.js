e4c42d02a49b9acfbff4f7f679a1d483712ca3be
Łukasz Samcik <l.samcik@gmail.com>
2023-04-06 13:47:59 +0200

    a

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,8 +30,8 @@ module.exports = {
 				gradient2: "hsl(335, 100%, 65%)",
 			},
 			backgroundImage: {
-				mobile: "url('/images/bg-mobile.png')",
-				desktop: "url('/images/bg-desktop.png')",
+				mobile: "url('images/bg-mobile.png')",
+				desktop: "url('images/bg-desktop.png')",
 			},
 		},
 	},
